808739af960b684c954f92ef0392f95936c82fcb
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-01 23:09:30 +0300

    Goobye, Car :(

--- src/main/java/com/adomasda/Garage.java
+++ src/main/java/com/adomasda/Garage.java
@@ -1,16 +1,5 @@
 package com.adomasda;
 
-class Car {
-    private String model;
-    public Car(String model) {
-        this.model = model;
-    }
-    @Override
-    public String toString() {
-        return model;
-    }
-}
-
 class Garage extends Room {
     private int carCapacity;
     private boolean isEmpty;
